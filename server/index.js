cdaa2bfaa4d15ffaa9555b2211e547a9e8301500
kushagra <kushagraa487@gmail.com>
2021-11-05 20:59:21 +0530

    Files Upload

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL ='mongodb+srv://Kushagra:Nikeaddidas%401@cluster0.9mx84.mongodb.net/Memories?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () =>
+        console.log(`Server running on port: ${PORT}`)
+    ))
+    .catch((error) => 
+        console.log(error.message)
+    );
